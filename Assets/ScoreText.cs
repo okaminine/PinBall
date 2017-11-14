b41633d1d7d44dc0751067be138a6458bc652d2b
ユニティクラス <wolf5052tokyo@yahoo.com>
2017-11-14 13:16:45 -0800

    PinBall　プロジェクト
    Lesson5 課題

--- Assets/ScoreText.cs
+++ Assets/ScoreText.cs
@@ -9,7 +9,7 @@ public class ScoreText : MonoBehaviour {
 	private GameObject tokuten;
 	private GameObject ball;
 
-//	private PointController ballpointcontroller;
+
 
 	// Use this for initialization
 	void Start () {
@@ -20,12 +20,6 @@ public class ScoreText : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
-//		ballpointcontroller = this.ball.GetComponent<PointController> ();
-//		int score = ballpointcontroller.score;
-
-			//GameoverTextにゲームオーバを表示
-//		this.tokuten.GetComponent<Text> ().text = score.ToString();
 
 		this.tokuten.GetComponent<Text> ().text = this.ball.GetComponent<PointController> ().score.ToString();
 	}
