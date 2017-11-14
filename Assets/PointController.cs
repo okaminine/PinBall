533a9ba535d111f66dc6f058c887177ddd6596e9
ユニティクラス <wolf5052tokyo@yahoo.com>
2017-11-13 23:19:19 -0800

    PinBall　プロジェクト
    Lesson5 課題

---
+++ Assets/PointController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointController : MonoBehaviour {
+
+	public int score = 0;
+
+
+		void Start () {
+
+			
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+		
+			
+
+
+		}
+
+		//衝突時に呼ばれる関数
+		void OnCollisionEnter(Collision collision) {
+
+		if (collision.gameObject.tag == "SmallStarTag") {
+				this.score += 10;
+		} else if (collision.gameObject.tag == "LargeStarTag") {
+				this.score += 20;
+		}else if(collision.gameObject.tag == "SmallCloudTag" || collision.gameObject.tag == "LargeCloudTag") {
+				this.score +=30;
+			}
+		}
+	}
